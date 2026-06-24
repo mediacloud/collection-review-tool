3a734c60effe506b2de2ccdf6c5ffe46c5c88155
Phil Budne <phil@regressive.org>
2026-06-24 16:38:30 -0400

    dokku-scripts/common.sh: restore code from web-search version

--- dokku-scripts/common.sh
+++ dokku-scripts/common.sh
@@ -7,12 +7,37 @@
 # please copy from one of those, or maybe it's time to have a repo
 # with a single/common version of the scripts??
 
-check_not_root() {
-    if [ "$(id -u)" -eq 0 ]; then
-        echo "$0: do not run as root" 1>&2
-        exit 1
-    fi
-}
+if [ "x$(whoami)" = xroot ]; then
+    # for crontab.sh
+    # no dokku function needed
+    alias check_root=true
+    check_not_root() {
+	echo "$0 must not be run as root" 1>&2
+	exit 1
+    }
+else
+    dokku() {
+	local _OK_FILE
+	_OK_FILE=$SCRIPT_DIR/.dokku-ssh-ok.$HOST
+	if [ ! -f $_OK_FILE ]; then
+	    # check ssh access working
+	    if ! ssh -n dokku@$FQDN version | grep -q '^dokku version'; then
+		echo "'ssh dokku@$FQDN' failed; need to run 'dokku ssh-keys' as root first" 1>&2
+		exit 1
+	    fi
+	    touch $_OK_FILE
+	fi
+	# NOTE! can't use localhost for ssh if user home directories NFS
+	# shared across servers (or else it will look like the host
+	# identity keeps changing)
+	ssh dokku@$FQDN "$@"
+    }
+    alias check_not_root=true
+    check_root() {
+	echo "$0 must be run as root" 1>&2
+	exit 1
+    }
+fi
 
 # Base app name for all instances
 APP_BASE=undp-collections-review
